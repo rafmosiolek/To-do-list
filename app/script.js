29073756392faaaf7c5ec3d3c330bfea29f9483c
RafM <rkjmosiolek@gmail.com>
2017-04-03 16:21:58 +0200

    global scope variables added

---
+++ app/script.js
@@ -0,0 +1,13 @@
+// Global scope variables
+
+var errorBox = document.getElementById('#error'); // error box displayed when user didn't enter a task
+var tasker = document.getElementByClass('.tasker'); // tasker header
+var inputTask = document.getElementById('#input-task'); // main input in a tasker header
+var addBtn = document.getElementByClass('.add-task'); // main add task button;
+
+var taskerBody = document.getElementByClass('.tasker-body'); // takser body div
+var deleteBtn = document.getElementByClass('.delete'); // delete task button
+var doneBtn = document.getElementByClass('.done'); // mark task as done button
+
+var tasks = document.getElementByClass('.tasks'); // new tasks
+

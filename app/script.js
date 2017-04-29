9422197af2e9bc347d1c8746a0c6d62d0bb88d4e
RafM <rkjmosiolek@gmail.com>
2017-04-29 18:49:20 +0200

    first event listener added

---
+++ app/script.js
@@ -0,0 +1,6 @@
+
+// user clicked on the add button
+// if there's any text in item#input, add that text to the to-do list
+document.getElementById("add").addEventListener("click", function(){
+    var value = document.getElementById("item").value;
+});
